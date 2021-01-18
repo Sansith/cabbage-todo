220b40232abee82e6ae0a1501fea97ee3705de24
Seniya <seniyadissanayake@gmail.com>
2021-01-19 02:01:59 +0530

    checkButton story

--- src/atoms/TaskCheckButton.stories.tsx
+++ src/atoms/TaskCheckButton.stories.tsx
@@ -2,9 +2,21 @@ import { Meta } from "@storybook/react/types-6-0";
 import TaskCheckButton from "./TaskCheckButton";
 
 export default {
-  title: "Example/TaskCheckButton",
+  title: "TaskCheckButton",
   component: TaskCheckButton,
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
 } as Meta;
+export const NotChecked = () => (
+  <TaskCheckButton
+    isCompleted={false}
+    label='Task'
+    onPress={() => null}
+  ></TaskCheckButton>
+);
+
+export const Checked = () => (
+  <TaskCheckButton
+    isCompleted={true}
+    label='Task'
+    onPress={() => null}
+  ></TaskCheckButton>
+);
